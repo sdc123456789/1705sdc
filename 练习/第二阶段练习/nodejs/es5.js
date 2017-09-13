fac83c69674bee845e770d8a68da0727f76f3d67
sdc <sdc@example.com>
2017-09-13 09:22:17 +0800

    额V

---
+++ 练习/第二阶段练习/nodejs/es5.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var a = 10011;
+var aaa = function aaa() {
+  return 10;
+};
+var aaa = function aaa() {
+  return 120;
+};
+var b = function b() {
+  return 102;
+};
---
+++ 练习/第二阶段练习/nodejs/js/es6.js
@@ -0,0 +1,4 @@
+let a = 10011
+var aaa=()=>10
+var aaa=()=>120
+var b=()=>102
